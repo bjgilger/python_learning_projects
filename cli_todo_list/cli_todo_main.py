f7c99ae673a09466e15de090516bc176433f2ce9
johng <john.gilger@gmail.com>
2024-08-13 16:57:58 -0700

    added edit feature - Day 4

--- cli_todo_list/cli_todo_main.py
+++ cli_todo_list/cli_todo_main.py
@@ -1,14 +1,14 @@
 """
-Title: TODO LIST CLI
+Title: Todo List CLI
 Author:  John Gilger
-This is a simple CLI application for creating TODO lists.
+This is a simple CLI application for creating Todo lists.
 """
 
 
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -18,6 +18,11 @@ while True:
         case "show":
             for item in todos:
                 print(item.title())
+        case "edit":
+            number = int(input("Enter the number of the todo item to edit: "))
+            number = number -1
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
 
