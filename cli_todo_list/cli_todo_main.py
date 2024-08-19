4dc528c5ec647f8b67d027a74666731df8d214b4
johng <john.gilger@gmail.com>
2024-08-19 16:18:42 -0700

    Optimized with if-elif - Day 9

--- cli_todo_list/cli_todo_main.py
+++ cli_todo_list/cli_todo_main.py
@@ -9,59 +9,60 @@ while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a task: ") + '\n'
+    if "add" in user_action:
+        todo = user_action[4:]
 
-            with open("tasks.txt", "r") as file:
-                todos = file.readlines()
+        with open("tasks.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            with open("tasks.txt", "w") as file:
-                file.writelines(todos)
+        with open("tasks.txt", "w") as file:
+            file.writelines(todos)
 
-        case "show":
-            with open("tasks.txt", "r") as file:
-                todos = file.readlines()
+    elif "show" in user_action:
+        with open("tasks.txt", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                item = item.strip("\n")
-                print(f"{index + 1}: {item.title()}")
+        for index, item in enumerate(todos):
+            item = item.strip("\n")
+            print(f"{index + 1}: {item.title()}")
 
-        case "edit":
-            number = int(input("Enter the number of the task item to edit: "))
-            number = number - 1
+    elif "edit" in user_action:
+        number = int(user_action[5:])
+        number = number - 1
 
-            with open("tasks.txt", "r") as file:
-                todos = file.readlines()
+        with open("tasks.txt", "r") as file:
+            todos = file.readlines()
 
-            new_todo = input("Enter the new task: ")
-            todos[number] = new_todo + '\n'
+        new_todo = input("Enter the new task: ")
+        todos[number] = new_todo + '\n'
 
-            with open("tasks.txt", "w") as file:
-                file.writelines(todos)
+        with open("tasks.txt", "w") as file:
+            file.writelines(todos)
 
-        case "complete":
-            number = int(input("Enter the number of the task item completed: "))
+    elif "complete" in user_action:
+        number = int(user_action[9:])
 
-            with open("tasks.txt", "r") as file:
-                todos = file.readlines()
-                index = number - 1
+        with open("tasks.txt", "r") as file:
+            todos = file.readlines()
+            index = number - 1
 
-            task_to_remove = todos[index].strip('\n')
-            print(task_to_remove)
-            number = number - 1
-            todos.pop(index)
+        task_to_remove = todos[index].strip('\n')
+        number = number - 1
+        todos.pop(index)
 
-            with open("tasks.txt", "w") as file:
-                file.writelines(todos)
+        with open("tasks.txt", "w") as file:
+            file.writelines(todos)
 
-            message = f"Task {task_to_remove} completed."
-            print(message)
+        message = f"Task {task_to_remove} completed."
+        print(message)
 
-        case "exit":
-            break
+    elif "exit" in user_action:
+        break
+
+    else:
+        print("Invalid input")
 
 print("Bye!")
 
