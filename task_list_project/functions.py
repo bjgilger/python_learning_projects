aa02e92acf59dc7a8f89abad12c7f9a2ee2876aa
johng <john.gilger@gmail.com>
2024-08-30 16:26:08 -0700

    added gui window - Day 16

--- task_list_project/functions.py
+++ task_list_project/functions.py
@@ -3,15 +3,22 @@ Title: Functions for CLI Task List project
 Author:  John Gilger
 These functions are for a simple CLI application for creating task lists.
 """
-def get_todos(filepath="tasks.txt"):
+
+FILEPATH = "tasks.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return a list of tasks """
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
         return todos_local
 
 
-def write_todos(todos_arg, filepath="tasks.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write a task list to a text file """
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
+
+if __name__ == "__main__":
+    print(get_todos())
---
+++ task_list_project/gui_task_list.py
@@ -0,0 +1,16 @@
+"""
+Title: GUI Task List
+Author:  John Gilger
+This is a simple GUI application for creating task lists.
+"""
+
+import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Add a task")
+input_box = sg.InputText(tooltip="Enter a task")
+add_button = sg.Button("Add")
+
+window = sg.Window('A Simple Task List', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
