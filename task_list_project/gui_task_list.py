9b2a0dde059a9984c0cbd6cf00658c550b96f621
johng <john.gilger@gmail.com>
2024-08-30 17:01:27 -0700

    start zipper exercise - Day 16.1

--- task_list_project/gui_task_list.py
+++ task_list_project/gui_task_list.py
@@ -11,6 +11,9 @@ label = sg.Text("Add a task")
 input_box = sg.InputText(tooltip="Enter a task")
 add_button = sg.Button("Add")
 
-window = sg.Window('A Simple Task List', layout=[[label], [input_box, add_button]])
+window = sg.Window('A Simple Task List', layout=[
+    [label],
+    [input_box, add_button]
+])
 window.read()
 window.close()
