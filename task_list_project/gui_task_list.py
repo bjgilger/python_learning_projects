e6dc5a6f39c6927325c1a79624c6c743b24cdeed
johng <john.gilger@gmail.com>
2024-09-03 12:03:14 -0700

    implement Add button - Day 17

--- task_list_project/gui_task_list.py
+++ task_list_project/gui_task_list.py
@@ -8,12 +8,27 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Add a task")
-input_box = sg.InputText(tooltip="Enter a task")
+input_box = sg.InputText(tooltip="Enter a task", key="TASK")
 add_button = sg.Button("Add")
 
-window = sg.Window('A Simple Task List', layout=[
-    [label],
-    [input_box, add_button]
-])
-window.read()
+window = sg.Window('A Simple Task List',
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20)
+                   )
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_task = values["TASK"] + '\n'
+            todos.append(new_task)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
